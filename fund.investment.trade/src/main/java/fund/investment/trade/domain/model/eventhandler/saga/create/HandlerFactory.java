d80ea8f038e5acb315ceb2db4ffc8713e99577eb
dongkw <dongkewei@ysstech.com>
2020-10-22 10:45:32 +0800

    format

--- fund.investment.trade/src/main/java/fund/investment/trade/domain/model/eventhandler/saga/create/HandlerFactory.java
+++ fund.investment.trade/src/main/java/fund/investment/trade/domain/model/eventhandler/saga/create/HandlerFactory.java
@@ -16,17 +16,24 @@ import java.util.*;
 @Slf4j
 public class HandlerFactory {
 
-    private static final Map<Set<OrderSagaStatus>, Set<IStatusHandler>> handlerMap = new HashMap<>();
+    private static final Map<String, Set<IStatusHandler>> handlerMap = new HashMap<>();
 
     public static void register(List<OrderSagaStatus> statuses, IStatusHandler handler) {
-        Set<IStatusHandler> set = handlerMap.getOrDefault(statuses, new HashSet<>());
+        Set<IStatusHandler> set = handlerMap.getOrDefault(list2str(statuses), new HashSet<>());
         set.add(handler);
-        handlerMap.put(new HashSet<>(statuses), set);
+        handlerMap.put(list2str(statuses), set);
     }
 
     public void handler(OrderVo vo) {
         log.debug("now orderId,{} status:{},", vo.getOrderId(), vo.getStatuses());
-        handlerMap.getOrDefault(vo.getStatuses(), new HashSet<>()).forEach(t -> t.handler(vo));
+        handlerMap.getOrDefault(set2str(vo.getStatuses()), new HashSet<>()).forEach(t -> t.handler(vo));
     }
 
+    private static String list2str(List<OrderSagaStatus> statuses) {
+        return statuses.stream().map(Objects::toString).sorted().reduce("", (start, t) -> start + "-" + t);
+    }
+
+    private static String set2str(Set<OrderSagaStatus> statuses) {
+        return statuses.stream().map(Objects::toString).sorted().reduce("", (start, t) -> start + "-" + t);
+    }
 }
