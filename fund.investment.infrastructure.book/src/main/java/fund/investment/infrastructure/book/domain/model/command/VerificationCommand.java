c8fc9a065100f063d5f7cc691defa7a33f68aefa
dongkw <dongkewei@ysstech.com>
2020-10-22 17:19:00 +0800

    cmd%evt

--- fund.investment.infrastructure.book/src/main/java/fund/investment/infrastructure/book/domain/model/command/VerificationCommand.java
+++ fund.investment.infrastructure.book/src/main/java/fund/investment/infrastructure/book/domain/model/command/VerificationCommand.java
@@ -1,11 +1,13 @@
 package fund.investment.infrastructure.book.domain.model.command;
 
 import fund.investment.infrastructure.common.DomainCommand;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author dongkw
  * @Date 2020/10/10、10:30 上午
  **/
+@NoArgsConstructor
 public class VerificationCommand extends DomainCommand {
     public VerificationCommand(String id) {
         super(id);
--- fund.investment.infrastructure.compliance/src/main/java/fund/investment/infrastructure/compliance/domain/model/event/ComplianceEvent.java
+++ fund.investment.infrastructure.compliance/src/main/java/fund/investment/infrastructure/compliance/domain/model/event/ComplianceEvent.java
@@ -1,11 +1,13 @@
 package fund.investment.infrastructure.compliance.domain.model.event;
 
 import fund.investment.infrastructure.common.DomainEvent;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author dongkw
  * @Date 2020/10/10、10:14 上午
  **/
+@NoArgsConstructor
 public class ComplianceEvent extends DomainEvent {
     public ComplianceEvent(String id) {
         super(id);
