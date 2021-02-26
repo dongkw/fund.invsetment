da961f9fbedac48fea7e468529e8c426a994bd77
dongkw <dongkewei@ysstech.com>
2021-02-26 14:27:50 +0800

    end

--- fund.investment.app/fund.investment.app.exchange.stock/fund.investment.app.exchange.stock.server/src/main/java/fund/investment/app/exchange/stock/server/ExchangeStockApplication.java
+++ fund.investment.app/fund.investment.app.exchange.stock/fund.investment.app.exchange.stock.server/src/main/java/fund/investment/app/exchange/stock/server/ExchangeStockApplication.java
@@ -2,12 +2,16 @@ package fund.investment.app.exchange.stock.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author dongkw
  * @Date 2021/2/25、4:19 下午
  **/
 @SpringBootApplication
+@ComponentScan({"fund.investment.basic.common",
+        "fund.investment.basic.instruction.server",
+        "fund.investment.basic.trade.server"})
 public class ExchangeStockApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeStockApplication.class);
--- fund.investment.gateway/fund.investment.gateway.server/src/main/java/fund/investment/gateway/server/GatewayApplication.java
+++ fund.investment.gateway/fund.investment.gateway.server/src/main/java/fund/investment/gateway/server/GatewayApplication.java
@@ -1,8 +1,15 @@
 package fund.investment.gateway.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author dongkw
  * @Date 2021/2/24、2:55 下午
  **/
+@SpringBootApplication
 public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
 }
